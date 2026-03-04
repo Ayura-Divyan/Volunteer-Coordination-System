ac350cd7cc1ae028a930150655fd4ae5f7c1e53a
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 23:19:28 +0530

    Assigned getter setters created

--- src/Volunteer.java
+++ src/Volunteer.java
@@ -9,7 +9,7 @@ public class Volunteer {
     private String name;
     private String taskAssigned;
     private String priorityLevel;
-    private Boolean assigned;
+    private boolean assigned;
 
     //Overloaded constructor - used to take user inputs and assign them to the attributes
     public Volunteer(String volunteerId, String name, String taskAssigned, String priorityLevel) {
@@ -17,6 +17,7 @@ public class Volunteer {
         this.name = name;
         this.taskAssigned = taskAssigned;
         this.priorityLevel = priorityLevel;
+        this.assigned = false;
     }
 
     //Getters
@@ -24,10 +25,12 @@ public class Volunteer {
     public String getName() {return name;}
     public  String getTaskAssigned() {return taskAssigned;}
     public String getPriorityLevel() {return priorityLevel;}
+    public boolean isAssigned() {return assigned;}
 
     //Setters
     public void setVolunteerId(String volunteerId) {this.volunteerId = volunteerId;}
     public void setName(String name) {this.name = name;}
     public void setTaskAssigned(String taskAssigned) {this.taskAssigned = taskAssigned;}
-    public  void setPriorityLevel(String priorityLevel) {this.priorityLevel = priorityLevel;}
+    public void setPriorityLevel(String priorityLevel) {this.priorityLevel = priorityLevel;}
+    public void setAssigned(boolean assigned) {this.assigned = assigned;}
 }
