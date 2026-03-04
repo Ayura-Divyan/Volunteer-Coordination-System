5688ac922ca70062ae84423b34ba66785014da0b
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 23:45:35 +0530

    Constructor for the coordinator added

--- src/VolunteerCoordination.java
+++ src/VolunteerCoordination.java
@@ -1,2 +1,16 @@
+import datastructures.*;
+
 public class VolunteerCoordination {
+    private LinkedList<Volunteer> allVolunteers;
+    private Queue<Volunteer> highPriorityQueue;
+    private Queue<Volunteer> mediumPriorityQueue;
+    private Queue<Volunteer> lowPriorityQueue;
+
+    //Constructor
+    public VolunteerCoordination() {
+        this.allVolunteers = new LinkedList<>();
+        this.highPriorityQueue = new Queue<>();
+        this.mediumPriorityQueue = new Queue<>();
+        this.lowPriorityQueue = new Queue<>();
+    }
 }
