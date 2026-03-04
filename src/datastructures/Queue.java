2ecedaa8f43736257225d8294ddbcc7c61e50011
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 21:05:49 +0530

    Enqueue method completed

--- src/datastructures/Queue.java
+++ src/datastructures/Queue.java
@@ -2,15 +2,18 @@ package datastructures;
 
 public class Queue<T> {
     //Fields
-    private Node<T> head;
-    private Node<T> tail;
-
+    private Node<T> front;
+    private Node<T> rear;
+    
     public void enqueue(T item) {
-        Node<T> queueNode = new Node<>(item); //The loop results in O(1)
+        Node<T> queueNode = new Node<>(item); //The use of a node results in O(1)
 
-        if (head == null) {
-            head = tail = queueNode;
-            return;
+        if (front == null) {
+            front = queueNode;
+            rear = queueNode;
+        } else {
+            rear.next = queueNode;
+            rear = queueNode;
         }
     }
 }
