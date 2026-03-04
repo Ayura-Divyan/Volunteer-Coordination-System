3193a282e137eacfa6491eb7b4866691a237ef59
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 22:04:15 +0530

    Stack methods completed

--- src/datastructures/Stack.java
+++ src/datastructures/Stack.java
@@ -1,5 +1,27 @@
 package datastructures;
 
-public class Stack {
-    private Node<T> front
+public class Stack<T> {
+    //Field
+    private Node<T> top;
+
+    public void push(T item) {
+        Node<T> stackNode = new Node<>(item); //The use of a node results in O(1)
+        top = top.next;
+        top.next = stackNode;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            System.out.println("The stack is empty");
+            return null;
+        }
+        T data = top.data;
+        top = top.next;
+
+        return data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
 }
