77b9a7e573fe5c72fa3e93228e5d27a8bdc043c3
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 21:19:35 +0530

    Dequeue method created

--- src/datastructures/Queue.java
+++ src/datastructures/Queue.java
@@ -4,7 +4,8 @@ public class Queue<T> {
     //Fields
     private Node<T> front;
     private Node<T> rear;
-    
+
+    //Method to enqueue the volunteers the queue
     public void enqueue(T item) {
         Node<T> queueNode = new Node<>(item); //The use of a node results in O(1)
 
@@ -15,5 +16,10 @@ public class Queue<T> {
             rear.next = queueNode;
             rear = queueNode;
         }
+    }
+
+    //Method to dequeue an item at the front
+    public void dequeue() {
+
     }
 }
