70904432f6ca8991aad47843147c27ca0f125bed
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-04 22:51:35 +0530

    Priority types added, rest of the fields added

--- src/Volunteer.java
+++ src/Volunteer.java
@@ -1,6 +1,13 @@
 public class Volunteer {
+    //Priority types
+    public static final String PRIORITY_HIGH = "high";
+    public static final String PRIORITY_MEDIUM= "medium";
+    public static final String PRIORITY_LOW = "low";
+
     //Fields - Attributes of a volunteer
     private int volunteerId;
     private String name;
     private String taskAssigned;
+    private String priorityLevel;
+    private Boolean assigned;
 }
