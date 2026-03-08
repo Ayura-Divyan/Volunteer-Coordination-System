412e85ef3b313152cddc5779e4b6b70806f6af3f
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-08 16:25:27 +0530

    Volunteer registration complete

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,33 @@ public class Main {
         boolean running = true;
 
         while (running) {
+            //Main Menu
             System.out.println("\n-------Main Menu-------\n");
             System.out.println("Pick an option (Enter the option number)");
-            System.out.println("---------------------------");
+            System.out.println("----------------------------------------");
             System.out.println("1. Register a volunteer");
             System.out.println("2. Remove a volunteer");
             System.out.println("3. Search for a volunteer");
             System.out.println("4. Assign task to a volunteer");
-            System.out.println("5. Exit program");
+            System.out.println("5. Undo last action");
+            System.out.println("6. Exit program");
+            System.out.println("----------------------------------------");
+            System.out.println("Enter your choice (1-6): ");
+            int choice = input.nextInt();
 
+            //Process the user choice
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the volunteer ID: ");
+                    String id = input.nextLine();
+                    System.out.println("Enter the volunteer name: ");
+                    String name = input.nextLine();
+                    System.out.println("Enter the volunteer priority (HIGH/MEDIUM/LOW): ");
+                    String priority = input.nextLine();
+
+                    system.registerVolunteer(id, name, priority);
+                    System.out.println("Volunteer was successfully registered ");
+            }
         }
     }
 }
